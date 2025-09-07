cf573495806a01596e0126d2f6847a402e4d7928
Hoàng Tuân <61133822+hoangtuantk@users.noreply.github.com>
2025-09-07 16:57:17 +0700

    -

--- txt-to-xhtml/src/js/m_config.js
+++ txt-to-xhtml/src/js/m_config.js
@@ -5,7 +5,6 @@ export const config = {
   defaultHeader: `<?xml version="1.0" encoding="utf-8"?>\n<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">\n\n<html xmlns="http://www.w3.org/1999/xhtml">\n<head>\n  <title></title>\n  <link href="../Styles/stylesheet.css" rel="stylesheet" type="text/css"/>\n</head>\n<body>`,
   defaultFooter: `</body>\n</html>`,
   defaultFilterRules: [
-    { type: 'regular', name: '·', find: "·", replace: "", caseSensitive: false, wholeWord: false, enabled: false, lineMatchMode: 'contains' },
-    { type: 'regular', name: 'demo', find: "demo", replace: "demo", caseSensitive: false, wholeWord: false, enabled: false, lineMatchMode: 'contains' }
+    { type: 'regular', name: ' ', find: " ", replace: " ", caseSensitive: false, wholeWord: false, enabled: false, lineMatchMode: 'contains' }
   ],
 };
